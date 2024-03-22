308d2ab1dd8f9122b3f0639b4c128452eacb211c
anjie_f <amfarmer2022@gmail.com>
2024-03-21 23:08:27 -0500

    Running the function

--- js/script.js
+++ js/script.js
@@ -7,11 +7,16 @@ var tripInfo = function () {
     var totalBudget = Number(prompt('What is the todal budget?'));
     var accommodation = Number(prompt('What are your accomomodation costs?'));
     var numDays = Number(prompt('How many days does your trip last?'));
+
+    //call the function 
+    calcuateDailyBudget(totalBudget, accommodation, numDays);
 };
 
 //write a function to calcuate your daily budget
-
 var calcuateDailyBudget = function (totalBudget, accommodation, numDays) {
-    var daily = ((totalBudget - accommodation)/numDays).toFixed(2);
-    dailyBudget.innerText = "You can spend $${daily} a day on food and fun!";
+    var daily = ((totalBudget - accommodation) / numDays).toFixed(2);
+    dailyBudget.innerText = `You can spend $${daily} a day on food and fun!`;
 };
+
+//add a click event
+tripInfoButton.addEventListener("click", tripInfo);
