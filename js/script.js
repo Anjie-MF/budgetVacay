7f8d5408116162bf6fe8357d618c051dc6fcec5e
anjie_f <amfarmer2022@gmail.com>
2024-03-21 22:33:01 -0500

    Wrote function for user input

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 //select a paragraph & button 
 var tripInfoButton = document.querySelector(".trip-info");
 var dailyBudget = document.querySelector(".daily-budget");
+
+//write a function to get user input
+var tripInfo = function () {
+    var totalBudget = Number(prompt('What is the todal budget?'));
+    var accommodation = Number(prompt('What are your accomomodation costs?'));
+    var numDays = Number(prompt('How many days does your trip last?'));
+};
